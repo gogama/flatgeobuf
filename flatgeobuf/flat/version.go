d015056a58a127259cb038080484f639ec8e148f
Victor Schappert <victor.schappert@protonmail.com>
2023-07-04 09:13:22 -0700

    Export version numbers from package flat

--- flatgeobuf/flat/version.go
+++ flatgeobuf/flat/version.go
@@ -4,11 +4,26 @@
 
 package flat
 
-import _ "embed"
+import (
+	_ "embed"
+)
 
 var (
 	//go:embed "version-flatc.txt"
 	flatcVersion string
 	//go:embed "version-schema.txt"
 	schemaVersion string
+	// Version documents the software versions used to build package
+	// flat.
+	Version = struct {
+		// Flatc contains the version of the FlatBuffers compiler,
+		// flatc, used to build package flat.
+		Flatc string
+		// FlatGeobufSchema contains the version of the FlatGeobuf
+		// FlatBuffers schema used to build package flat.
+		FlatGeobufSchema string
+	}{
+		Flatc:            flatcVersion,
+		FlatGeobufSchema: schemaVersion,
+	}
 )
