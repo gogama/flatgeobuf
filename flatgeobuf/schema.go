fbaa8f9f06cc416014409069f09f45fb1a40f19e
Victor Schappert <victor.schappert@protonmail.com>
2023-10-11 07:13:57 -0700

    Document Schema interface

--- flatgeobuf/schema.go
+++ flatgeobuf/schema.go
@@ -6,10 +6,29 @@ package flatgeobuf
 
 import "github.com/gogama/flatgeobuf/flatgeobuf/flat"
 
-// TODO: Docs
+// Schema is a schema for FlatGeobuf's feature property format. It
+// documents the number of available properties (ColumnsLength) for a
+// feature and the metadata (flat.Column) for each property.
+//
+// Both the header structure (flat.Header) for a FlatGeobuf file and an
+// individual feature within the data section (flat.Feature) implement
+// Schema. When provided on the header table, the Schema applies to all
+// features in the file except those that have their own schema's. When
+// provided on an individual feature, the Schema applies only to that
+// feature.
+//
+// Use PropReader.ReadSchema to read all properties
 type Schema interface {
-	// TODO: Docs
+	// ColumnsLength returns the number of columns, i.e. properties, in
+	// the schema.
 	ColumnsLength() int
-	// TODO: Docs
+	// Columns obtains the metadata for the column, i.e. property, at a
+	// specific index.
+	//
+	// The index j may range from 0 to ColumnsLength()-1. The pointer
+	// obj must not be nil. On return, obj points to the property
+	// metadata (flat.Column) for property j and the return value is
+	// true. A return value of false indicates that no property metadata
+	// was found for the property at index j.
 	Columns(obj *flat.Column, j int) bool
 }
